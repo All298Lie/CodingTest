b71d51b37f5cc3497adce9c0e3d695e7e7c0bbf4
All298Lie <whdudfo333@naver.com>
2026-03-05 00:33:39 +0900

    3월 1주차 코딩테스트 문제 학/복습 (1/3)

---
+++ programmers/2018 KAKAO BLIND RECRUITMENT/NewsClustering_first_after.cpp
@@ -0,0 +1,70 @@
+﻿#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+// 문자열을 unordered_map 컨테이너를 이용해 집합 형태로 변환하는 함수
+unordered_map<string, int> toSet(string str)
+{
+    for (auto& c : str) c = tolower(c);
+
+    unordered_map<string, int> _set;
+
+    for (int i = 0; i < str.length() - 1; i++)
+    {
+        if (isalpha(str[i]) && isalpha(str[i + 1]))
+        {
+            _set[str.substr(i, 2)]++;
+        }
+    }
+
+    return _set;
+}
+
+// 집합 A,B의 합집합 원소 개수를 리턴하는 함수
+int count_union(unordered_map<string, int>& set1, unordered_map<string, int>& set2)
+{
+    int result = 0;
+    
+    unordered_map<string, int> _union;
+    for (const auto& pair : set1) _union[pair.first] += pair.second;
+    for (const auto& pair : set2) _union[pair.first] = max(_union[pair.first], pair.second);
+
+    for (const auto& pair : _union) result += pair.second;
+
+    return result;
+}
+
+// 집합 A,B의 교집합 원소 개수를 리턴하는 함수
+int count_intersection(unordered_map<string, int>& set1, unordered_map<string, int>& set2)
+{
+    int result = 0;
+    
+    unordered_map<string, int> _intersection;
+    for (const auto& pair : set1)
+    {
+        if (set2.find(pair.first) != set2.end())
+        {
+            result += min(pair.second, set2[pair.first]);
+        }
+    }
+
+    return result;
+}
+
+int solution(string str1, string str2) {
+    // 두 문자열을 집합으로 만들기
+    unordered_map<string, int> set1 = toSet(str1);
+    unordered_map<string, int> set2 = toSet(str2);
+
+    // 두 집합 A, B가 공집합일 경우 J(A, B) = 1로 계산
+    if (set1.size() == 0 && set2.size() == 0) return 1 * 65'536;
+
+    // 자카드 유사도 계산
+    float c1 = count_intersection(set1, set2);
+    float c2 = count_union(set1, set2);
+
+    float answer = c1 / c2;
+
+    return (int)(answer * 65'536.0f);
+}
---
+++ programmers/2018 KAKAO BLIND RECRUITMENT/NewsClustering_first_before.cpp
@@ -0,0 +1,82 @@
+﻿#include <string>
+#include <vector>
+#include <set> // set 컨테이너, multiset 컨테이너 사용
+
+using namespace std;
+
+// 문자열을 vector 컨테이너를 이용해 집합 형태로 변환하는 함수
+vector<string> toSet(string str)
+{
+    for (auto& c : str) c = tolower(c);
+
+    multiset<string> mts;
+
+    for (int i = 1; i < str.length(); i++)
+    {
+        if (!(isalpha(str[i - 1]) && isalpha(str[i]))) continue;
+
+        mts.insert(str.substr(i - 1, 2));
+    }
+
+    return vector<string>(mts.begin(), mts.end());
+}
+
+// 집합 A,B의 합집합 원소 개수를 리턴하는 함수
+int count_union(vector<string> set1, vector<string> set2)
+{
+    int result = 0;
+    multiset<string> mts1(set1.begin(), set1.end());
+    multiset<string> mts2(set2.begin(), set2.end());
+
+    multiset<string> _union(mts1.begin(), mts1.end());
+    for (auto str : mts2) _union.insert(str);
+
+    set<string> _un(_union.begin(), _union.end());
+
+    for (auto str : _un)
+    {
+        result += max(mts1.count(str), mts2.count(str));
+    }
+
+    return result;
+}
+
+// 집합 A,B의 교집합 원소 개수를 리턴하는 함수
+int count_intersection(vector<string> set1, vector<string> set2)
+{
+    int result = 0;
+    multiset<string> mts1(set1.begin(), set1.end());
+    multiset<string> mts2(set2.begin(), set2.end());
+
+    multiset<string> _intersection(mts1.begin(), mts1.end());
+    for (auto str : mts2) _intersection.insert(str);
+
+    set<string> _is(_intersection.begin(), _intersection.end());
+
+    for (auto str : _is)
+    {
+        if (_intersection.count(str) < 2) continue;
+        if (min(mts1.count(str), mts2.count(str)) < 1) continue;
+
+        result += min(mts1.count(str), mts2.count(str));
+    }
+
+    return result;
+}
+
+int solution(string str1, string str2) {
+    // 두 문자열을 집합으로 만들기
+    vector<string> set1 = toSet(str1);
+    vector<string> set2 = toSet(str2);
+
+    // 두 집합 A, B가 공집합일 경우 J(A, B) = 1로 계산
+    if (set1.size() == 0 && set2.size() == 0) return 1 * 65536;
+
+    // 자카드 유사도 계산
+    float c1 = count_intersection(set1, set2);
+    float c2 = count_union(set1, set2);
+
+    float answer = c1 / c2;
+
+    return (int)(answer * 65536.0f);
+}
