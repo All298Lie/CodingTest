073f6d1434ee5a2cfd70103d242c1dac0e7840ca
All298Lie <whdudfo333@naver.com>
2026-03-08 16:40:53 +0900

    3월 1주차 코딩테스트 문제 학/복습 (2/3)
    연습문제 옹알이(2)

---
+++ programmers/practice questions/Babbling2_first_after.cpp
@@ -0,0 +1,40 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> a = { "aya", "ye", "woo", "ma" };
+
+// 재귀함수를 통해 단어를 계속 탐색
+int change(const string& str, const string& before, int index)
+{
+    // 인덱스가 문자열 뒤를 가르키고 있을 경우 통과
+    if (index == str.size()) return 1;
+
+    for (const string& word : a)
+    {
+        if (index + word.length() > str.length()) continue;
+
+        if (str.compare(index, word.length(), word) == 0) {
+            if (word == before) return 0;
+
+            return change(str, word, index + word.length());
+        }
+    }
+
+    return 0;
+}
+
+// 문제
+int solution(vector<string> babbling)
+{
+    int answer = 0;
+
+    for (auto& str : babbling) answer += change(str, "", 0);
+
+    return answer;
+}
+
+// 풀이 방식 : for문으로 단어를 순회하며, compare() 함수를 통해 단어를 비교
+//             단어가 적은 단순 문제라 for문을 사용해 순회하는 방식 사용.
+//             단어가 많아질 경우, unordered_set 컨테이너를 사용해 기존에 쓰던 substr()함수와 find()함수가 효율적
---
+++ programmers/practice questions/Babbling2_first_before.cpp
@@ -0,0 +1,48 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> a = { "aya", "ye", "woo", "ma" };
+
+// 재귀 함수를 통해 단어를 계속 탐색
+string change(string str, string before)
+{
+    if (str.length() == 0) return "A";
+    else if (str.length() == 1) return "B";
+
+    auto it = find(a.begin(), a.end(), str.substr(0, 2));
+    if (it != a.end())
+    {
+        if (*it == before) return "B";
+
+        return change(str.substr(2), *it);
+    }
+
+    it = find(a.begin(), a.end(), str.substr(0, 3));
+    if (str.length() > 2 && it != a.end())
+    {
+        if (*it == before) return "B";
+
+        return change(str.substr(3), *it);
+    }
+
+    return "B";
+}
+
+// 문제
+int solution(vector<string> babbling)
+{
+    int answer = 0;
+
+    for (string str : babbling)
+    {
+        if (change(str, "") == "A") answer++;
+    }
+
+    return answer;
+}
+
+// 풀이 방식 : find()함수와 substr()을 통해 문자열을 끊어서 일치하는 단어가 있는지 확인
+// 문제점 : substr()과 재귀 함수 실행과정에서 불필요한 메모리 복사가 됨
