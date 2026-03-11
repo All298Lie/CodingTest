fae9a6020b8277d0a392eab65e584f49bc7a4e48
All298Lie <whdudfo333@naver.com>
2026-03-12 00:19:44 +0900

    3월 2주차 코딩테스트 문제 학/복습  (1/3)
    안전지대

---
+++ programmers/beginner/SafeZone_first_after.cpp
@@ -0,0 +1,47 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+bool minesweeper(const vector<vector<int>>& board, int row, int col)
+{
+    for (int dir_row = -1; dir_row <= 1; dir_row++)
+    {
+        for (int dir_col = -1; dir_col <= 1; dir_col++)
+        {
+            int next_row = dir_row + row;
+            int next_col = dir_col + col;
+
+            if (next_row < 0 || next_row >= board.size()) continue;
+            if (next_col < 0 || next_col >= board[0].size()) continue;
+
+            if (board[next_row][next_col] == 1)
+            {
+                return false;
+            }
+        }
+    }
+    
+    return true;
+}
+
+int solution(vector<vector<int>> board)
+{
+    int answer = 0;
+
+    for (int row = 0; row < board.size(); row++)
+    {
+        for (int col = 0; col < board[0].size(); col++)
+        {
+            answer += minesweeper(board, row, col);
+        }
+    }
+
+    return answer;
+}
+
+// 문제 해결 : 다중 for문을 사용해서 배열을 탐색.
+// 폭탄은 3x3 범위를 가지고 있으므로, 안전 구역 또한 주변 3x3 범위를 확인하면 안전한지 확인 가능.
+// 배운점 :
+// 1. 배열 탐색은 재귀 방식이 어울리지 않음.
+// 2. 함수로 값을 호출할 때, 컨테이너의 경우 주소 참조를, 일반 자료형은 값 복사를 하는 것이 좋음.
---
+++ programmers/beginner/SafeZone_first_before.cpp
@@ -0,0 +1,52 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+// 재귀 함수를 통해 처음부터 마지막까지 확인
+int minesweeper(vector<vector<int>>& board, const int& x, const int& y)
+{
+    if (x == board.front().size())
+    {
+        if (y == board.size())
+        {
+            int empty = 0;
+
+            for (const auto& line : board)
+                for (const auto& p : line)
+                    if (p == 0) empty++;
+
+            return empty;
+        }
+
+        // 해당 행을 다 확인했다면 다음 열로 이동
+        return minesweeper(board, 0, y + 1);
+    }
+
+    if (board[x][y] == 1)
+    {
+        for (int a = y - 1; a < y + 2; a++)
+        {
+            for (int b = x - 1; b < x + 2; b++)
+            {
+                if (a < 0 || a >= board.size()) continue;
+                if (b < 0 || b >= board.front().size()) continue;
+
+                if (board[b][a] == 0) board[b][a] = 2;
+            }
+        }
+    }
+
+    // 다음 행으로 이동하여 확인
+    return minesweeper(board, x + 1, y);
+}
+
+int solution(vector<vector<int>> board)
+{
+    return minesweeper(board, 0, 0);
+}
+
+// 문제 해결 : 재귀 함수 방식으로 모든 행렬을 확인 및 폭탄이 발견될 경우, 주변 8곳의 값을 변경. 
+// 마지막에 값이 0인 모든 부분의 값 더해서 리턴
+// 문제점 : 재귀의 깊이때문에 공간복잡도와 효율성이 좋지 않음.
+// 이런 문제(배열 탐색)의 해결방식은 재귀 함수가 아닌 방향 배열을 이용하여 다중 for문을 통해 좌표를 확인하는 것이 정석
