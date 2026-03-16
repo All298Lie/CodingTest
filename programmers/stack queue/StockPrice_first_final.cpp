6258361a5054c517309b0a83ee73f13ed414b2c5
All298Lie <whdudfo333@naver.com>
2026-03-16 22:51:57 +0900

    3월 3주차 코딩테스트 문제 학/복습 (1/3)
    이전주차에서 배운 단조 스택 복습을 위한 문제 풀이

---
+++ programmers/stack queue/StockPrice_first_final.cpp
@@ -0,0 +1,37 @@
+﻿#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer(prices.size(), 0);
+
+    vector<int> st;
+
+    // 배열 순회
+    for (int index = 0; index < prices.size(); index++)
+    {
+        // 스택이 비어있지 않을 경우 while문 반복
+        while (st.empty() == false)
+        {
+            if (prices[st.back()] <= prices[index]) break;
+
+            // 가격이 떨어진 주식의 기간 저장
+            answer[st.back()] = index - st.back();
+            st.pop_back();
+        }
+
+        st.push_back(index);
+    }
+
+    // 끝까지 가격이 떨어지지 않은 주식의 기간 저장
+    while (st.empty() == false)
+    {
+        answer[st.back()] = prices.size() - st.back() - 1;
+        st.pop_back();
+    }
+
+    return answer;
+}
+
+// 풀이 방식 : 단조 스택을 사용하는 방식으로, 스택 방식의 자료구조에 인덱스를 저장하여 다음 인덱스를 넣을때마다
+// 스택의 끝부분에 담긴 인덱스와 비교하며 가격이 떨어졌을 경우 주식의 기간을 계산하는 방식
