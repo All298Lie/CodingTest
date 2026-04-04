476d0b2ad9ec0eb22425e153d7c9a0152e8a20e9
All298Lie <whdudfo333@naver.com>
2026-04-04 18:11:51 +0900

    4월 1주차 코딩테스트 문제 학/복습 (3/3)

---
+++ programmers/beginner/OverlappingLines_first_after.cpp
@@ -0,0 +1,43 @@
+﻿#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> lines) {
+    int answer = 0;
+
+    // 1. 선분을 점의 좌표와 선의 증감으로 분리 표현
+    vector<pair<int, int>> events;
+    for (auto& line : lines)
+    {
+        events.push_back({ line[0], 1 }); // 선분의 시작점. 이 이후로 선 개수가 증가됨
+        events.push_back({ line[1], -1 }); // 선분의 끝점. 이 이후로 선 개수가 감소됨
+    }
+
+    // 2. 좌표를 기준으로 오름차순 정렬
+    sort(events.begin(), events.end()); 
+
+    int activeLines = 0;
+    int lastPoint = events[0].first;
+
+    // 3. 정렬된 이벤트를 순서대로 스윕
+    for (auto& event : events)
+    {
+        if (activeLines >= 2 ) // 겹쳐있던 선분이 2개 이상이었을 경우, 길이만큼 추가
+        {
+            answer = answer + event.first - lastPoint;
+        }
+
+        // 선분 개수 갱신 및 현재 위치 저장
+        activeLines = activeLines + event.second;
+        lastPoint = event.first;
+    }
+
+    return answer;
+}
+
+// 문제 해결 :
+// 기존에 생각했던 방식의 선분의 점과 점을 넘어들며 선분이 겹쳐있을 경우 선분의 길이만큼 더하는 방식이 기하에서 사용되는 라인 스위핑 알고리즘으로,
+// 좌표와 해당 좌표에 일어난 이벤트(선의 증감)를 쌓아 정렬하여, 선이 2개 겹치는 구간을 마지막 탐색 좌표와 현재 탐색 좌표를 통해 선분의 길이를 구하여 계산하는 문제였음
+
+// 해당 문제를 통해 라인 스위핑이란 알고리즘을 알게되어 좋은 계기가 되었다고 생각함
---
+++ programmers/beginner/OverlappingLines_first_before.cpp
@@ -0,0 +1,45 @@
+﻿#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> lines) {
+    int answer = 0;
+
+    // 1. 시작점과 끝점 확인
+    int first_point = lines[0][0]; // 시작점
+    int last_point = lines[0][1]; // 끝점
+    for (int index = 1; index < lines.size(); index++)
+    {
+        first_point = min(first_point, lines[index][0]);
+        last_point = max(last_point, lines[index][1]);
+    }
+
+    // 2. first_point부터 last_point까지 순회하며 선분이 겹치는지 확인
+    for (int point = first_point; point < last_point; point++)
+    {
+        int check = 0; // 선분이 겹치는지 확인할때 사용할 변수
+
+        for (auto line : lines) // 선분 순회
+        {
+            // point부터 point + 1까지 잇는 선분과 해당 선분이 겹칠 경우, 체크
+            if (line[0] <= point && line[1] >= point + 1)
+            {
+                check = check + 1;
+            }
+
+            if (check >= 2) break; // 2개 이상일 경우, 선분이 이미 겹치므로 빠르게 탈출
+        }
+
+        // 체크가 2개이상 일경우, 겹치는 선분이 존재하므로 추가
+        if (check >= 2) answer = answer + 1;
+    }
+
+    return answer;
+}
+
+// 문제 해결 :
+// 세 선분의 시작점을 비교해서 가장 낮은 시작점을 시발점으로 하고, 가장 높은 끝점을 종착점으로 하여 탐색을 진행.
+// 점을 기준으로 탐색을 하여, 현재 점과 다음 점을 잇는 선분과 겹치는 선분이 존재할 경우, 체크하여 겹치는 선분 2개가 존재할 경우 결과값 1 증가하도록 함
+
+// 기존에 생각했던 방식은 시발점을 찾고, 겹치는 선분이 존재할 경우 두 선분의 끝점 중 낮은 끝점까지의 길이를 구해서 결과값에 반영하고 해당 끝점을 시발점으로 하여 종착점까지 다시 탐색하는 알고리즘을 구상했음
+// -> 당장 코드 구현하기엔 복잡해질거같아 간단한 구조로 완성시켜버림
